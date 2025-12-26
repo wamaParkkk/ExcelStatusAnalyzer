ec7a3d94895874e808c4068698a2cdf4e46b4e34
Hyunsoo Park <zahir84@nate.com>
2025-12-26 10:22:53 +0900

    25/12/26 update

--- ExcelStatusAnalyzer/MainForm.cs
+++ ExcelStatusAnalyzer/MainForm.cs
@@ -134,36 +134,36 @@ namespace ExcelStatusAnalyzer
             this.Controls.Add(btnVisionRetryCount);
 
             // Alarm Pivot
-            var btnAlarmPivot = new Button { Text = "Alarm 집계", Left = 225, Top = 687, Width = 200, Height = 32 };
+            var btnAlarmPivot = new Button { Text = "Alarm 집계", Left = 15, Top = 724, Width = 200, Height = 32 };
             btnAlarmPivot.Click += (s, e) => new AlarmPivotForm().Show();
             this.Controls.Add(btnAlarmPivot);
 
             // Alarm Pivot2
-            var btnAlarmPivot2 = new Button { Text = "Alarm 집계(KTCB(White))", Left = 225, Top = 724, Width = 200, Height = 32 };
+            var btnAlarmPivot2 = new Button { Text = "Alarm 집계(KTCB(White))", Left = 15, Top = 724, Width = 200, Height = 32 };
             btnAlarmPivot2.Click += (s, e) => new AlarmPivotForm2().Show();
             this.Controls.Add(btnAlarmPivot2);
 
             // Alarm Pivot3
-            var btnAlarmPivot3 = new Button { Text = "Alarm 집계(KTCB-모델별(White))", Left = 435, Top = 724, Width = 200, Height = 32 };
+            var btnAlarmPivot3 = new Button { Text = "Alarm 집계(KTCB-모델별(White))", Left = 225, Top = 724, Width = 200, Height = 32 };
             btnAlarmPivot3.Click += (s, e) => new AlarmPivotForm3().Show();
             this.Controls.Add(btnAlarmPivot3);
 
             // Alarm Pivot4
-            var btnAlarmPivot4 = new Button { Text = "Alarm 집계(KTCB-MTBA(White))", Left = 645, Top = 724, Width = 200, Height = 32 };
+            var btnAlarmPivot4 = new Button { Text = "Alarm 집계(KTCB-MTBA(White))", Left = 435, Top = 724, Width = 200, Height = 32 };
             btnAlarmPivot4.Click += (s, e) => new AlarmPivotForm4().Show();
             this.Controls.Add(btnAlarmPivot4);
 
             // Alarm Pivot5
-            var btnAlarmPivot5 = new Button { Text = "Alarm 집계(KTCB-모델별 MTBA(White))", Left = 855, Top = 724, Width = 300, Height = 32 };
+            var btnAlarmPivot5 = new Button { Text = "Alarm 집계(KTCB-모델별 MTBA(White))", Left = 645, Top = 724, Width = 300, Height = 32 };
             btnAlarmPivot5.Click += (s, e) => new AlarmPivotForm5().Show();
             this.Controls.Add(btnAlarmPivot5);
 
             // Daily tracker
-            var btnDailyTracker = new Button { Text = "Daily Tracker(KTCB-모델별)", Left = 1065, Top = 724, Width = 300, Height = 32 };
+            var btnDailyTracker = new Button { Text = "Daily Tracker(KTCB-모델별)", Left = 955, Top = 724, Width = 200, Height = 32 };
             btnDailyTracker.Click += (s, e) => new DailyTrackerPivotForm().Show();
             this.Controls.Add(btnDailyTracker);
 
-            var btnShiftWeb = new Button { Text = "교대별 장비 상태 집계", Left = 435, Top = 687, Width = 200, Height = 32 };
+            var btnShiftWeb = new Button { Text = "교대별 장비 상태 집계", Left = 15, Top = 761, Width = 200, Height = 32 };
             btnShiftWeb.Click += (s, e) => new ShiftWebSummaryForm().Show();
             this.Controls.Add(btnShiftWeb);
         }
